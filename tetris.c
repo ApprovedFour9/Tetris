ad41dd12ee9f4d03f872dde8c800718d33a64240
ApprovedFour9 <approved4nine@gmail.com>
2019-05-22 20:22:38 +0700

    Edit source file:Add structs

--- tetris.c
+++ tetris.c
@@ -5,3 +5,51 @@
 #include <signal.h>
 #include <time.h>
 #include <fcntl.h>
+
+
+
+struct tetris {
+	char **game;
+	int w;
+	int h;
+	int gameover
+	struct tetris_block {
+		char data[5][5];
+		int w;
+		int h;
+	} current;
+	int x;
+	int y;
+};
+
+struct tetris_block blocks[] =
+{
+    {{"##", 
+         "##"},
+    2, 2
+    system("color E0");
+    },
+    {{" X ",
+         "XXX"},
+    3, 2
+    system("color A2");
+    },
+    {{"@@@@"},
+        4, 1},
+    {{"OO",
+         "O ",
+         "O "},
+    2, 3
+    system("color B3");
+    },
+    {{"&&",
+         " &",
+         " &"},
+    2, 3
+    system("color F7");
+    },
+    {{"ZZ ",
+         " ZZ"},
+    3, 2
+    system("color E6");}
+};
